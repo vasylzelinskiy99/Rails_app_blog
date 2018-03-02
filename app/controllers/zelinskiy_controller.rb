fdf023a9554590971fd2d8ed969c278adbef47ec
zelinskiy <vasiazel96@gmail.com>
2018-03-02 13:26:51 +0200

    Controllers config + routes

---
+++ app/controllers/zelinskiy_controller.rb
@@ -0,0 +1,8 @@
+class ZelinskiyController < ApplicationController
+  def home
+
+  end
+  def about
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'zelinskiy/home' ,to:  'zelinskiy#home'
+  get 'zelinskiy/about' ,to: 'zelinskiy#about'
 end
